8f50165aa81b2ae62f79c19e6e6f405ef3d990e4
kersh2000 <bradleykershaw333@gmail.com>
2023-06-08 01:54:12 +0100

    Added root api html

--- server.js
+++ server.js
@@ -10,8 +10,49 @@ const app = express();
 const { PORT = 5000 } = process.env;
 
 app.use(cors({origin: '*'}));
-
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  const title = 'Welcome to Colour Palette API';
+
+  const css = `
+    <style>
+      body {
+        background-color: #111;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        font-family: 'Arial Black', sans-serif;
+      }
+
+      .title {
+        font-size: 8vw;
+        font-weight: bold;
+        text-align: center;
+        background: linear-gradient(to right, #fc7fb2, #ff9e7c, #ffe070, #a4e57d, #77cdff, #c884e8);
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
+      }
+
+    </style>
+  `;
+
+  const html = `
+    <html>
+      <head>
+        <title>Welcome to Colour Palette API</title>
+        ${css}
+      </head>
+      <body>
+        <h1 class="title">${title}</h1>
+      </body>
+    </html>
+  `;
+
+  res.send(html);
+});
 
 app.get('/api', (req, res) => {
   res.status(200).send({
